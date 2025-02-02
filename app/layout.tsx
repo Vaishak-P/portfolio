8dcf1092ff23369a4b7ba6a38969397064b1d0f3
Vaishak-P <vaishakpanakkat@gmail.com>
2025-02-02 15:22:43 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Sora } from "next/font/google";
+import "./globals.css";
+import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
+
+const font = Sora({
+  weight:['100', '200', '300', '400', '500', '600', '700', '800'],
+  subsets:['latin'],
+});
+
+export const metadata: Metadata = {
+  title: "Vaishak P",
+  description: "Aspiring React Developer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={font.className}
+      >
+        <ResponsiveNav></ResponsiveNav>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from '@/components/Home/Home';
+import ResponsiveNav from '@/components/Home/Navbar/ResponsiveNav';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Home></Home>
+    </div>
+  )
+}
+
+export default HomePage;
