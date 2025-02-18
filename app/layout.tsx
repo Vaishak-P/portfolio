fc90787f4be63f052fb313be11b4482a8547876c
Vaishak-P <vaishakpanakkat@gmail.com>
2025-02-18 23:21:05 +0530

    poppins font set

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
-import { Sora } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 import Footer from "@/components/Home/Footer/Footer";
 import ScrollToTop from "@/components/Helper/ScrollToTop";
+import AnimatedCursor from "react-animated-cursor"
 
-const font = Sora({
+const font = Poppins({
   weight:['100', '200', '300', '400', '500', '600', '700', '800'],
   subsets:['latin'],
 });
@@ -21,10 +22,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="custom-scrollbar">
       <body
         className={font.className}
       >
+        <div className="hidden md:block">
+          <AnimatedCursor innerSize={8} outerSize={24} innerScale={2} outerScale={2} outerAlpha={0} innerStyle ={{backgroundColor:'white'}} outerStyle={{border:'3px solid white'}}/>
+        </div>
         <ResponsiveNav></ResponsiveNav>
         {children}
         <Footer></Footer>
