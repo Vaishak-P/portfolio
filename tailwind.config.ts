7c75f2e87cbf205e5b8bc41f2357f00f4682722c
Vaishak-P <vaishakpanakkat@gmail.com>
2025-02-26 18:22:26 +0530

    new green color scheme defined

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,17 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend:{
+      colors:{
+        highDark:'#232931',
+        lowDark:'#393E46',
+        pureBlack:'#1B1B1B	',
+        primary:'#11998e',
+        secondary:'#38ef7d',
+        lowWhite:'#EEEEEE'
+      }
+    }
+  },
   plugins: [],
 } satisfies Config;
