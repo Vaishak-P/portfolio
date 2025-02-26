cb3625e001451edb411201d513b95b46d130178b
Vaishak-P <vaishakpanakkat@gmail.com>
2025-02-26 18:22:57 +0530

    aos implemented

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client';
+import React, { useEffect } from 'react'
 import Hero from './Hero/Hero'
 import About from './About/About'
 import Services from './Services/Services'
@@ -6,8 +7,23 @@ import Project from './Project/Project'
 import Skills from './Skills/Skills'
 import Spotlight from './Spotlight/Spotlight'
 import Contact from './Contact/Contact'
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 
 const Home = () => {
+  useEffect(() => {
+    const initAOSasync =async () => {
+      await import("aos");
+      AOS.init({
+        duration:1000,
+        easing:'ease',
+        once:true,
+        anchorPlacement:'top-bottom',
+      });
+    };
+    AOS.init();
+  },[]);
   return (
     <div>
       <Hero/> 
