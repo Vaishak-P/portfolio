1d0cf7a91ca9c2f528f6f8eca155fea8b45993c7
Vaishak-P <vaishakpanakkat@gmail.com>
2025-02-26 18:28:43 +0530

    color scheme applied and aos added

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -5,34 +5,51 @@ import { FaDownload } from 'react-icons/fa';
 
 const Hero = () => {
   return (
-    <div className="w-full pt-[4vh] md:pt-[12vh] h-screen bg-gray-800 overflow-hidden relative">
+    <div className="w-full pt-[4vh] md:pt-[12vh] h-screen bg-highDark overflow-hidden relative">
       <div className="flex justify-center flex-col w-4/5 h-full mx-auto">
         <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-12">
           {/* Text Content  */}
           <div>
-            <h1 className="text-2xl md:text-3xl lg:text-4xl mb-5 text-gray-300 font-semibold">
+            <h1
+              data-aos="fade-left"
+              className="text-2xl md:text-3xl lg:text-4xl mb-5 text-lowWhite font-semibold"
+            >
               {BaseInfo.name}
             </h1>
             {/* //text-bg is custom class defined on globals.css  */}
             <h1
+              data-aos="fade-right"
+              data-aos-delay="100"
               className="text-bg text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold 
-                    md:leading-[3rem] lg:leading-[3.5rem] xl:leading-[4rem] text-white"
+                    md:leading-[3rem] lg:leading-[3.5rem] xl:leading-[4rem] text-lowWhite"
             >
               {BaseInfo.position}
             </h1>
-            <p className="mt-6 text-sm md:text-base text-white text-opacity-60">
+            <p
+              data-aos="fade-left"
+              data-aos-delay="200"
+              className="mt-6 text-sm md:text-base text-lowWhite text-opacity-60"
+            >
               {BaseInfo.description}
             </p>
-            <button
-              className="md:px-8 md:py-2.5 px-6 py-1.5 text-white font-semibold text-sm md:text-lg
-             transition-all duration-200 rounded-lg mt-8 bg-blue-700 hover:bg-blue-900 flex items-center space-x-2"
-            >
-              <span>Download CV</span>
-              <FaDownload />
-            </button>
+            <a href="/Resume.pdf" download="Vaishak_P_Resume.pdf">
+              <button
+                data-aos="zoom-in"
+                data-aos-delay="300"
+                className="md:px-8 md:py-2.5 px-6 py-1.5 text-lowWhite font-semibold text-sm md:text-lg
+             transition-all duration-200 rounded-lg mt-8 bg-primary hover:bg-secondary hover:text-highDark flex items-center gap-x-3"
+              >
+                Download Resume
+                <FaDownload />
+              </button>
+            </a>
           </div>
           {/* Image Content */}
-          <div className="mx-auto hidden lg:block rounded-[3rem] border-[3.5px] border-blue-950 overflow-x-hidden">
+          <div
+            data-aos="zoom-in"
+            data-aos-delay="400"
+            className="mx-auto hidden lg:block rounded-[3rem] shadow-lg  shadow-primary overflow-x-hidden"
+          >
             <Image
               src={BaseInfo.profilePic}
               alt={BaseInfo.name}
--- components/Home/Services/Services.tsx
+++ components/Home/Services/Services.tsx
@@ -5,15 +5,19 @@ import ServiceCard from './ServiceCard';
 
 const Services = () => {
   return (
-    <div id='services' className="pt-16 pb-16 bg-green-950">
+    <div id='services' className="pt-20 pb-16 bg-highDark">
       <SectionHeading>Services</SectionHeading>
       <div className='grid grid-cols-1 sm:grid-cols-3 lg:grid-cold-3 
       xl:grid-cold-4 gap-10 w-[80%] mx-auto items-center mt-20'>
-        {servicesData.map((service)=>{
+        {servicesData.map((service,i)=>{
             return (
-            <div key={service.id}>
+            <div key={service.id}
+            data-aos='fade-left'
+            data-aos-anchor-placement='top-center'
+            data-aos-delay={`${i*150}`}
+            >
                  <ServiceCard service={service}/>
-            </div>
+            </div> 
             );
         })}
       </div>
